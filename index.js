a879f132df6965e8c48ab5dd03521b6a5a5923cd
Sandy FedoraX <ctu@ku.edu>
2020-07-15 23:57:29 -0500

    Update the time every minute
    Signed-off-by: Sandy FedoraX <ctu@ku.edu>

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-document.addEventListener("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded", () => {
     let time = document.querySelector("#time");
     let sub = document.querySelector("#sub");
     const day = 24 * 60 * 60 * 1000;
 
-    let now = new Date();
-    let start = new Date(now.getFullYear(), 0, 0)
-    var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
+    setTime = () => {
+	let now = new Date();
+	let start = new Date(now.getFullYear(), 0, 0);
+	var diff = now - start +
+	    (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000;
+	
+	time.innerText =
+	    Math.floor(diff / day) + "; " + (now.getFullYear() + 10000) + " H.E.";
+	sub.innerText = (now.getHours() * 100 + now.getMinutes())
+	    .toString()
+	    .padStart(4, "0");
+    }
 
-    time.innerText = Math.floor(diff / day) + "; " + (now.getFullYear() + 10000) + " H.E.";
-    sub.innerText = (now.getHours() * 100 + now.getMinutes()).toString().padStart(4, "0");
+    setTime();
+    setInterval(setTime, 60 * 1000);
 });
